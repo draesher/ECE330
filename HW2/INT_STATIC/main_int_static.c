d824453df2a2d4d51240c85338fb217bfdec9875
draesher <78426036+draesher@users.noreply.github.com>
2021-02-14 22:13:42 -0700

    Add files via upload

---
+++ HW2/INT_STATIC/main_int_static.c
@@ -0,0 +1,29 @@
+/* File: main_int_static.c */
+#include "int_static.h"
+
+int main() 
+{
+  int r, c, i, j;
+  int val;
+  matrix test, trans;
+  std::cout << "How many rows? ";
+  std::cin >> r;
+  std::cout << "How many columns? ";
+  std::cin >> c;
+  test = creat_initval(r,c,0);
+  std::cout << std::endl << "enter elements of matrix: " << std:: endl;
+  for(i = 0; i<r; i++) {
+	for(j=0; j<c; j++) {
+		std::cout << "Enter element for row [" << i +1 << ']' << " col [" << j +1 << ']' << ": ";
+  		std::cin >> val;
+  		assign(i,j, &test, val);
+	}
+  }
+  
+  std::cout << "\n\t Your Matrix: ";
+  matrix_print(test);
+  std::cout << "\n Your Transposed Matrix: ");
+  trans = transpose(test);
+  matrix_print(trans);
+  return 0;
+}
